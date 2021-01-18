64fd93894c889f92a18610510066a6e61f836dda
bugsb <manishianand@gmail.com>
2021-01-18 19:40:10 +0530

    after backend

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask,request,render_template,redirect
+import pymongo
+
 
 app =  Flask(__name__)
-l=['ola','nick']
+# app.config["MONGO_URI"] = "mongodb+srv://admin:raina111@cluster0.rg8a7.mongodb.net/mydb?retryWrites=true&w=majority"
+# mongo = PyMongo(app)
+myclient = pymongo.MongoClient("mongodb+srv://admin:raina111@cluster0.rg8a7.mongodb.net/mydb?retryWrites=true&w=majority")
+mydb = myclient["mydb"]
+mycol = mydb["bucket"]
+
+# mycol.insert_one(mydict)
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -10,13 +18,18 @@ def index():
 def submit():
 	if request.method =='POST':
 		data = request.form.get('text')
-		l.append(data)
+		myDict = {}
+		myDict["name"] = data
+		mycol.insert_one(myDict)
 	return redirect('/')
 
 @app.route('/see',methods=['GET','POST'])
 def see():
-	
-	data = l
+	data = []
+	myquery = { "name": { "$regex": "^[a-z]|^[A-Z]" } }
+	for i in mycol.find(myquery):
+		data.append(i["name"])
+		data = sorted(data)
 	return render_template('base.html',data=data)
 if __name__ == '__main__':
 	app.run(debug=True)
